24bc348c27d46cedae3634afb63e20064b566fde
machida-yosuke <machida-yosuke@kayac.com>
2024-05-08 12:29:04 +0900

    雛形

--- udemy/main.tf
+++ udemy/main.tf
@@ -1,17 +1,21 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "hello-world" {
-  ami = "ami-0ab3794db9457b60a"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "HelloWorld"
-  }
+variable "project" {
+  type = string
+}
 
-  user_data = <<EOF
-#!/bin/bash
-amazon-linux-extras install -y nginx1.12
-systemctl start nginx
-EOF
+variable "environment" {
+  type = string
 }
